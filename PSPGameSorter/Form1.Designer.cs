9a889b47899b8d7111a2337a7fb01bcdcb82b210
José Antonio Barranquero Fernández <joseantbarranquero@gmail.com>
2020-11-10 22:51:34 +0100

    Agregar archivos de proyecto.

---
+++ PSPGameSorter/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace PSPGameSorter
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmbDrive = new System.Windows.Forms.ComboBox();
+            this.btnScan = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cmbDrive
+            // 
+            this.cmbDrive.FormattingEnabled = true;
+            this.cmbDrive.Location = new System.Drawing.Point(13, 13);
+            this.cmbDrive.Name = "cmbDrive";
+            this.cmbDrive.Size = new System.Drawing.Size(64, 21);
+            this.cmbDrive.TabIndex = 0;
+            // 
+            // btnScan
+            // 
+            this.btnScan.Location = new System.Drawing.Point(95, 11);
+            this.btnScan.Name = "btnScan";
+            this.btnScan.Size = new System.Drawing.Size(75, 23);
+            this.btnScan.TabIndex = 1;
+            this.btnScan.Text = "Scan drive";
+            this.btnScan.UseVisualStyleBackColor = true;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnScan);
+            this.Controls.Add(this.cmbDrive);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "frmMain";
+            this.Text = "PSP Game Sorter";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbDrive;
+        private System.Windows.Forms.Button btnScan;
+    }
+}
+
---
+++ PSPGameSorter/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace PSPGameSorter
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        /**
+         *  Obtain drives available and show them in combobox
+         */
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            DriveInfo[] allDrives = DriveInfo.GetDrives();
+
+            foreach (DriveInfo drive in allDrives)
+            {
+                cmbDrive.Items.Add(drive.Name);
+            }
+        }
+    }
+}
